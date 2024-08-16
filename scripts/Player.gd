75c9f738697f220b84eabfba0dacdef23bff8f37
jam54 <git@jam54.com>
2024-08-16 21:17:39 +0200

    Statically type variables instead of dynamically in `Player.gd`

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 ## This script is used to control and move the camera around in the play area
 
-@export var speed = 400
+@export var speed := 400
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	var input_direction := Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
 func _physics_process(delta):
