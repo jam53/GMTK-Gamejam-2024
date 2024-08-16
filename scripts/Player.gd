0a1e550a1c7c75537f239364b5c90c29b241d240
jam54 <git@jam54.com>
2024-08-16 20:31:14 +0200

    Add `Lawn` area + `Player` with camera to move around in the lawn

---
+++ scripts/Player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+## This script is used to control and move the camera around in the play area
+
+@export var speed = 400
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
