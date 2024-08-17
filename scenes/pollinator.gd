1eb6c854466bc00867d8329210199178ff8faa24
Aqua-sc <108478185+Aqua-sc@users.noreply.github.com>
2024-08-17 18:39:31 +0200

    Added text with honey amount

--- scenes/pollinator.gd
+++ scenes/pollinator.gd
@@ -6,6 +6,7 @@ extends Node2D
 @onready var boids_component = $BoidsComponent
 @onready var pollinate_timer = $PollinateTimer
 @onready var hive_timer = $HiveTimer
+@onready var game_manager = %GameManager
 
 var _flower_found := false
 var _in_hive := false
@@ -46,5 +47,6 @@ func _on_pollinate_timer_timeout():
 
 func _on_hive_timer_timeout():
 	print("Hivetime finished")
+	game_manager.update_honey(5)
 	_in_hive = false
 	boids_component.lure = null
---
+++ scripts/game_manager.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var honey_amount := 0
+@onready var honey_amount_label = $honeyamountlabel
+
+func update_honey(delta: int):
+	honey_amount += delta
+	honey_amount_label.text = "Honey_amount: " + str(honey_amount)
+	
