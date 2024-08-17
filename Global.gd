49ebc6e473e49180e2e4abc3fd0f1d74f911be7c
PJDeSmijter <pieterjan.desmijter@gmail.com>
2024-08-17 19:03:05 +0200

    bees inside the lawn
    todo
    continuous generation
    bee attack

--- Global.gd
+++ Global.gd
@@ -2,3 +2,4 @@
 extends Node
 
 var flowers: Array = []
+var bees : Array = []
--- scripts/bee_spawner.gd
+++ scripts/bee_spawner.gd
@@ -5,16 +5,16 @@ extends Node2D
 @export var spawn_area: ReferenceRect
 
 @export_category("Flowers to spawn")
-@export var flower_scene: PackedScene
+@export var bee_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Global.flowers = []
-	if flower_scene:
-		generate_flowers(flower_scene, initial_amount)
-		print("Flowers generated: ", Global.flowers.size())
+	Global.bees = []
+	if bee_scene:
+		generate_flowers(bee_scene, initial_amount)
+		print("Flowers generated: ", Global.bees.size())
 	else:
-		print("flower_scene is not assigned!")
+		print("bee_scene is not assigned!")
 
 # Generates a random position within the spawn_area
 func generate_random_position() -> Vector2:
@@ -23,20 +23,20 @@ func generate_random_position() -> Vector2:
 	return Vector2(randf_range(top_left.x, bottom_right.x), randf_range(bottom_right.y, top_left.y))
 
 # Instantiates a flower at a given position.
-func spawn_flower(flower: PackedScene, pos: Vector2) -> Node:
-	if flower:
-		var flowerInstance = flower.instantiate()
-		flowerInstance.position = pos
-		add_child(flowerInstance)
-		Global.flowers.append(flowerInstance)
+func spawn_flower(bee: PackedScene, pos: Vector2) -> Node:
+	if bee:
+		var beeInstance = bee.instantiate()
+		beeInstance.position = pos
+		add_child(beeInstance)
+		Global.bees.append(beeInstance)
 		print("Flower spawned at position: ", pos)
-		return flowerInstance
+		return beeInstance
 	else:
-		print("Cannot instantiate flower, flower is null!")
+		print("Cannot instantiate bee, bee is null!")
 		return null
 
 # Generates a specified number of flowers of the given PackedScene type
-func generate_flowers(flower: PackedScene, amount := 1):
+func generate_flowers(bee: PackedScene, amount := 1):
 	for i in range(0, amount):
 		var pos = generate_random_position()
-		spawn_flower(flower, pos)
+		spawn_flower(bee, pos)
