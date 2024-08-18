0549b644c73a65bd87463a7bb5eccbc1ee146f55
PJDeSmijter <pieterjan.desmijter@gmail.com>
2024-08-18 10:31:37 +0200

    one spawner script

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -4,13 +4,14 @@ extends Node2D
 @export var initial_amount: int = 5
 @export var spawn_area: ReferenceRect
 
-@export_category("Flowers to spawn")
-@export var flower_scene: PackedScene
+@export_category("Items to spawn")
+@export var scene: PackedScene
+@export var group: String
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if flower_scene:
-		generate_flowers(flower_scene, initial_amount)
+	if scene:
+		generate_items(scene, initial_amount)
 
 # Generates a random position within the spawn_area
 func generate_random_position() -> Vector2:
@@ -19,18 +20,18 @@ func generate_random_position() -> Vector2:
 	return Vector2(randf_range(top_left.x, bottom_right.x), randf_range(bottom_right.y, top_left.y))
 
 # Instantiates a flower at a given position.
-func spawn_flower(flower: PackedScene, pos: Vector2) -> Node:
+func spawn_item(flower: PackedScene, pos: Vector2) -> Node:
 	if flower:
-		var flowerInstance = flower.instantiate()
-		flowerInstance.position = pos
-		add_child(flowerInstance)
-		add_to_group("flowers")
-		return flowerInstance
+		var instance = flower.instantiate()
+		instance.position = pos
+		add_child(instance)
+		add_to_group(group)
+		return instance
 	else:
 		return null
 
 # Generates a specified number of flowers of the given PackedScene type
-func generate_flowers(flower: PackedScene, amount := 1):
+func generate_items(flower: PackedScene, amount := 1):
 	for i in range(0, amount):
 		var pos = generate_random_position()
-		spawn_flower(flower, pos)
+		spawn_item(flower, pos)
