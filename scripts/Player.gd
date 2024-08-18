f004aea556f25539baa545daf7d68ea4d4c7e26f
jam54 <git@jam54.com>
2024-08-18 15:56:45 +0200

    Merge `Player.gd` and `Camera2D.gd` scripts which handled the camera movement using the keyboard and mouse respectively into a single script called `Player.gd`

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,12 +1,35 @@
 extends CharacterBody2D
 ## This script is used to control and move the camera around in the play area
 
-@export var speed := 500
+@export var camera_speed: float = 500 # Speed at which the camera moves 
+@export var margin: int = 25 # Margin for detecting when the mouse is out of bounds
 
+# Used to move the camera using the cursor
+func _process(delta: float) -> void:
+	var mouse_pos: Vector2 = get_viewport().get_mouse_position() # Get the mouse position relative to the viewport
+
+	var screen_size: Vector2 = get_viewport().get_visible_rect().size
+
+	var target_position: Vector2 = global_position
+
+	# Check if the mouse is outside the bounds of the screen
+	if mouse_pos.x < margin:
+		target_position.x -= camera_speed * delta
+	elif mouse_pos.x > screen_size.x - margin:
+		target_position.x += camera_speed * delta
+
+	if mouse_pos.y < margin:
+		target_position.y -= camera_speed * delta
+	elif mouse_pos.y > screen_size.y - margin:
+		target_position.y += camera_speed * delta
+
+	global_position = global_position.lerp(target_position, 1)
+
+# Used to move the camera using the keyboard
 func get_input():
-    var input_direction := Input.get_vector("left", "right", "up", "down")
-    velocity = input_direction * speed
+	var input_direction := Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * camera_speed
 
 func _physics_process(delta):
-    get_input()
-    move_and_slide()
+	get_input()
+	move_and_slide()
