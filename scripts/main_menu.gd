4e64777348d79880063c2ef77aaf6c33fc29cbb6
jam54 <git@jam54.com>
2024-08-19 20:34:42 +0200

    Add functionality to the "How to play" button on the main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,4 +1,12 @@
 extends Node2D
 
+@export var how_to_play_window: Window
+
 func _on_play_button_down():
-	get_tree().change_scene_to_file("res://scenes/lawn.tscn")
+	get_tree().change_scene("res://scenes/lawn.tscn")
+
+func _on_how_to_play_button_down():
+	how_to_play_window.show()
+
+func _on_how_to_play_close_requested():
+	how_to_play_window.hide()
