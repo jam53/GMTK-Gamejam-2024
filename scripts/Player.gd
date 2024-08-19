7325df191bdd3fb6d7718782cc1be35f15317fbc
jam54 <git@jam54.com>
2024-08-19 16:08:22 +0200

    Made it so the camera movement using the cursor can be disabled by setting a `bool` flag on `player.gd`

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,27 +3,29 @@ extends CharacterBody2D
 
 @export var camera_speed: float = 500 # Speed at which the camera moves 
 @export var margin: int = 25 # Margin for detecting when the mouse is out of bounds
+@export var enable_camera_movement_with_cursor: bool = true
 
 # Used to move the camera using the cursor
 func _process(delta: float) -> void:
-	var mouse_pos: Vector2 = get_viewport().get_mouse_position() # Get the mouse position relative to the viewport
+	if enable_camera_movement_with_cursor:
+		var mouse_pos: Vector2 = get_viewport().get_mouse_position() # Get the mouse position relative to the viewport
 
-	var screen_size: Vector2 = get_viewport().get_visible_rect().size
+		var screen_size: Vector2 = get_viewport().get_visible_rect().size
 
-	var target_position: Vector2 = global_position
+		var target_position: Vector2 = global_position
 
-	# Check if the mouse is outside the bounds of the screen
-	if mouse_pos.x < margin:
-		target_position.x -= camera_speed * delta
-	elif mouse_pos.x > screen_size.x - margin:
-		target_position.x += camera_speed * delta
+		# Check if the mouse is outside the bounds of the screen
+		if mouse_pos.x < margin:
+			target_position.x -= camera_speed * delta
+		elif mouse_pos.x > screen_size.x - margin:
+			target_position.x += camera_speed * delta
 
-	if mouse_pos.y < margin:
-		target_position.y -= camera_speed * delta
-	elif mouse_pos.y > screen_size.y - margin:
-		target_position.y += camera_speed * delta
+		if mouse_pos.y < margin:
+			target_position.y -= camera_speed * delta
+		elif mouse_pos.y > screen_size.y - margin:
+			target_position.y += camera_speed * delta
 
-	global_position = global_position.lerp(target_position, 1)
+		global_position = global_position.lerp(target_position, 1)
 
 # Used to move the camera using the keyboard
 func get_input():
