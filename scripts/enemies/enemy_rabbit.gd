56292cd7c48136734b31d75df017ad35db4aac21
PJDeSmijter <pieterjan.desmijter@gmail.com>
2024-08-20 13:46:53 +0200

    enemy death sounds 💀🎵

--- scripts/enemies/enemy_rabbit.gd
+++ scripts/enemies/enemy_rabbit.gd
@@ -1,4 +1,9 @@
 extends CharacterBody2D
 
+@onready var musicplayer = $AudioStreamPlayer
+
 func _on_health_component_died():
+	if musicplayer:
+		musicplayer.play()
+	await get_tree().create_timer(0.1).timeout
 	queue_free()
